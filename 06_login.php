26c501fa1e95ece232cfc0fa8388bec26c28f111
Quezia Brito <quezia.p.silva6@aluno.senai.br>
2025-10-17 11:27:43 -0300

    Atualização dos nomes dos arquivos.

---
+++ 06_login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login de Usuário</title>
+</head>
+<body>
+    <form method="post" action="">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" required
+</body>
+</html>
